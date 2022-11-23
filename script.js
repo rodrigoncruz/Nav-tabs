a4716bca9a6e25c8a71494588826a263f0b4cd8b
Rodrigo Cruz <rodrigonascimentocruz@gmail.com>
2022-11-23 16:47:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-content section');
+tabContent[0].classList.add('ativo');
+
+const activeTab = (index) => {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+
+    tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    });
+})
+
+
